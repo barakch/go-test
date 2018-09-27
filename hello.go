66628336af48d6131ec4606e2b605ab39dce5b1f
Barak Hacham <barakh@jfrog.com>
2018-09-27 10:32:21 +0300

    Deploy Go project

---
+++ hello.go
@@ -0,0 +1,13 @@
+package main // import "github.com/you/hello"
+
+import ("log"
+
+        "net/http"
+
+        "github.com/gorilla/mux")
+
+// our main function
+func main() {
+        router := mux.NewRouter()
+        log.Fatal(http.ListenAndServe(":8000", router))
+}
